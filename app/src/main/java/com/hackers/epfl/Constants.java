2c75ed153972fb7fe80ec015512300da4c4f0425
Filip Hrisafov <filip.hrisafov@gmail.com>
2014-05-11 08:52:39 +0200

    change the request param

--- app/src/main/java/com/hackers/epfl/Constants.java
+++ app/src/main/java/com/hackers/epfl/Constants.java
@@ -6,5 +6,5 @@ package com.hackers.epfl;
 public class Constants {
 	public static final String PARAM_BEACON_ID = "beaconID";
 	public static final String PARAM_MESSAGE = "message";
-	public static final String PARAM_REQUEST = "request";
+	public static final String PARAM_REQUEST = "requestPost";
 }
